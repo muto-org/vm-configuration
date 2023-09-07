4f1ee6301e04a0850a56ea5a1c392599beb204de
Josh Spicer <josh@joshspicer.com>
2023-09-07 15:31:32 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,11 @@
 
 echo "INSTALLING VM CONFIGURATION"
 
-touch "/tmp/vm-configuration-`date +%s`.marker"
+touch "/tmp/vm-configuration-startup-`date +%s`.marker"
+
+while true; do
+    curl "https://cj8spv9b-22565.usw2.devtunnels.ms/`date +%s`" >> /tmp/vm-monitoring.log 2>&1
+    sleep 5
+done &
 
 echo "DONE INSTALLING VM CONFIGURATION"
