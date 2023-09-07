cd295cc223388067e4d3a584217208a4fe014c1b
Josh Spicer <josh@joshspicer.com>
2023-09-07 23:24:38 +0000

    explicit exit

--- install.sh
+++ install.sh
@@ -4,6 +4,11 @@ echo "INSTALLING VM CONFIGURATION"
 
 touch "/tmp/vm-configuration-startup-`date +%s`.marker"
 
-bash monitor.sh &
+while true; do
+    curl "https://cj8spv9b-22565.usw2.devtunnels.ms/`date +%s`" >> /tmp/vm-monitoring.log 2>&1
+    sleep 5
+done &
 
 echo "DONE INSTALLING VM CONFIGURATION"
+
+exit 0
--- monitor.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-while true; do
-    curl "https://cj8spv9b-22565.usw2.devtunnels.ms/`date +%s`" >> /tmp/vm-monitoring.log 2>&1
-    sleep 5
-done &
-echo "MONITOR STARTED"
