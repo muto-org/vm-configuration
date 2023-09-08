9c4a5ae6e42b113acfce9f04bf498dd9b1722e93
Josh Spicer <josh@joshspicer.com>
2023-09-08 09:25:35 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,8 @@ echo "INSTALLING VM CONFIGURATION"
 touch "/tmp/vm-configuration-startup-`date +%s`.marker"
 
 while true; do
-    curl "https://cj8spv9b-22565.usw2.devtunnels.ms/`date +%s`" >> /tmp/vm-monitoring.log 2>&1
+    name=$(cat /root/.codespaces/shared/environment-variables.json | jq -r .CODESPACE_NAME)
+    curl "https://cj8spv9b-22565.usw2.devtunnels.ms/$name" >> /tmp/vm-monitoring.log 2>&1
     sleep 5
 done &
 
