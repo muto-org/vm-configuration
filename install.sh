50b10e7b2844556c30f38b4f7517711d45434edd
Josh Spicer <josh@joshspicer.com>
2023-09-28 11:13:54 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
 
-echo "INSTALLING VM CONFIGURATION"
-
-touch "/tmp/vm-configuration-startup-`date +%s`.marker"
-
-while true; do
-    name=$(cat /root/.codespaces/shared/environment-variables.json | jq -r .CODESPACE_NAME)
-    curl "https://znmd8wm1-25565.usw2.devtunnels.ms/$name" >> /tmp/vm-monitoring.log 2>&1
-    sleep 5
-done &
-
-echo "DONE INSTALLING VM CONFIGURATION"
-
-exit 0
+echo "exiting with 1 to simulate a script failure....."
+exit 1
