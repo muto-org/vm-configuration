909611ec12c52664431530b90f18422918cac289
Josh Spicer <josh@joshspicer.com>
2023-09-29 15:30:09 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,19 +12,9 @@ fi
 INSTALL_LOG=${BASE_FOLDER}/install.log
 echo "INSTALL_LOG=${INSTALL_LOG}"
 
-# if [ ! -f ${INSTALL_LOG} ]; then
-    touch ${INSTALL_LOG}
-    chmod a+rw ${INSTALL_LOG}
-    echo `date +%s` >> /tmp/vm-configuration-startup.marker
-    echo "Installing in a background process ..."
-    /usr/bin/nohup ./worker.sh -o ./onboard.py --base-folder ${BASE_FOLDER} &
-    echo $! > /tmp/vm-configuration.pid
-# else
-#     echo "already installed"
-#     if [ -f ./nohup.out ]; then
-#         cat ./nohup.out
-#     else
-#         cat ${INSTALL_LOG}
-#     fi
-#     exit 0
-# fi
+touch ${INSTALL_LOG}
+chmod a+rw ${INSTALL_LOG}
+echo `date +%s` >> /tmp/vm-configuration-startup.marker
+echo "Installing in a background process ..."
+/usr/bin/nohup ./worker.sh -o ./onboard.py --base-folder ${BASE_FOLDER} &
+echo $! > /tmp/vm-configuration.pid
