6f7e909f0fdc88dd81ec358b531fa5026265ec6b
Josh Spicer <josh@joshspicer.com>
2023-10-18 21:10:54 +0000

    nohupWithDevNull

--- install.sh
+++ install.sh
@@ -17,7 +17,9 @@ popd
 echo "[install.sh] Begin"
 touch "${BASE_FOLDER}/`date +%H:%M:%S`-vm-configuration-startup.marker"
 
-nohup ./helper.sh "$BASE_FOLDER" nohup01 10 &
+# nohup ./helper.sh "$BASE_FOLDER" nohup 10 &
+nohup ./helper.sh "$BASE_FOLDER" nohupWithDevNull 10 > /dev/null 2>&1 &
+# nohup ./helper.sh "$BASE_FOLDER" nohupWithDevNull 10 > "${BASE_FOLDER}/nohupWithDevNull" 2>&1 &
 
 echo "[install.sh] End"
 touch "${BASE_FOLDER}/`date +%H:%M:%S`-vm-configuration-shutdown.marker"
